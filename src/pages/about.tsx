f03f2bf940aa64140dc893b10f74adfeaead4db0
moneyandjelly <rmsdldy12@gmail.com>
2024-12-05 15:02:32 +0900

    feat: [이근] 콘텐츠 인트로 박스 추가

--- src/pages/about.tsx
+++
@@ -1,7 +0,0 @@
-function about() {
-    return (
-      <div>about 페이지 입니다.</div>
-    )
-  }
-  
-  export default about
--- src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-function main() {
-  return (
-    <div>main 페이지 입니다.</div>
-  )
-}
-
-export default main
