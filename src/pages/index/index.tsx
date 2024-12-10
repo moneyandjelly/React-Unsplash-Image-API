3646794e2b3175bbf0f8878867c8c2fc0eb70c17
moneyandjelly <rmsdldy12@gmail.com>
2024-12-10 16:50:31 +0900

    feat: [이근] recoil 추가

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,4 +1,6 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import { useRecoilValue } from 'recoil'
+import { imageData } from '@/recoil/selectors/imageSelector'
 import CommonHeader from '@components/common/header/CommonHeader'
 import CommonSearchBar from '@/components/common/searchBar/CommonSearchBar'
 import CommonNav from '@/components/common/navigation/CommonNav'
@@ -6,41 +8,17 @@ import CommonFooter from '@/components/common/footer/CommonFooter'
 import Card from './components/card'
 //CSS
 import styles from './styles/index.module.scss'
-import axios from 'axios'
 import { CardDTO } from './types/card'
 
 function index() {
-    const [imgUrls, setImgUrls] = useState([])
+    const imgSelector = useRecoilValue(imageData)
+    const [imgData, setImgData] = useState<CardDTO[]>([])
 
-    const getData = async () => {
-        const API_URL = 'https://api.unsplash.com/search/photos'
-        const API_KEY = "BhFRFMwiLQPQBgrcbQoM5ScoCL9KyCXJnLeUHSHkdyw"
-        const PER_PAGE = 30
-
-        const searchValue = 'Korea'
-        const pageValue = 100
-
-        try {
-            const res = await axios.get(`${API_URL}?query=${searchValue}&client_id=${API_KEY}&page=${pageValue}&per_page=${PER_PAGE}`)
-            console.log(res)
-
-            if (res.status === 200) {
-                setImgUrls(res.data.results)
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    const cardList = imgUrls.map((card: CardDTO) => {
+    const CARD_LIST = imgSelector.data.results.map((card: CardDTO) => {
         return (
             <Card data={card} key={card.id} />
         )
     })
-
-    useEffect(() => {
-        getData()
-    }, [])
 
     return (
         <div className={styles.page}>
@@ -61,7 +39,7 @@ function index() {
                     </div>
                 </div>
                 <div className={styles.page__contents__imageBox}>
-                    {cardList}
+                    {CARD_LIST}
                 </div>
             </div>
             {/* 공통 푸터 UI 부분 */}
