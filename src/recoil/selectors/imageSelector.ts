83f552d4cefef293c6026aad2c9618414a179b4f
moneyandjelly <rmsdldy12@gmail.com>
2024-12-10 17:01:27 +0900

    feat: [이근] API 키 암호화

--- src/recoil/selectors/imageSelector.ts
+++ src/recoil/selectors/imageSelector.ts
@@ -4,7 +4,7 @@ import { pageState } from '../atoms/pageState'
 
 import axios from 'axios'
 const API_URL = 'https://api.unsplash.com/search/photos'
-const API_KEY = "BhFRFMwiLQPQBgrcbQoM5ScoCL9KyCXJnLeUHSHkdyw"
+const API_KEY = import.meta.env.VITE_UNSPLASH_API_ACCESS_KEY
 const PER_PAGE = 30
 
 export const imageData = selector({
