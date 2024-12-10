44b17c47d91620912c786d2565aef70811d5743f
moneyandjelly <rmsdldy12@gmail.com>
2024-12-10 14:23:12 +0900

    feat: [이근] axios 추가

--- src/components/common/navigation/CommonNav.tsx
+++ src/components/common/navigation/CommonNav.tsx
@@ -23,9 +23,9 @@ function CommonNav() {
     })
 
     return (
-        <div className={styles.navigation}>
+        <nav className={styles.navigation}>
             {navLinks}
-        </div>
+        </nav>
     )
 }
 
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -5,8 +5,29 @@ import CommonFooter from '@/components/common/footer/CommonFooter'
 import Card from './components/card'
 //CSS
 import styles from './styles/index.module.scss'
-
+import axios from 'axios'
+import { useEffect } from 'react'
 function index() {
+    const getData = async () => {
+        const API_URL = 'https://api.unsplash.com/search/photos'
+        const API_KEY = "BhFRFMwiLQPQBgrcbQoM5ScoCL9KyCXJnLeUHSHkdyw"
+        const PER_PAGE = 30
+
+        const searchValue = 'Korea'
+        const pageValue = 100 
+
+        try{
+            const res = await axios.get(`${API_URL}?query=${searchValue}&client_id=${API_KEY}&page=${pageValue}&per_page=${PER_PAGE}`)
+            console.log(res)
+        }catch(error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
     return (
         <div className={styles.page}>
             {/* 공통 헤더 UI 부분 */}
