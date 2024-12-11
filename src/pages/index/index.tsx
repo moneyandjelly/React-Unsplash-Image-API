7b08b66d663db0fecc872da5f846bed7907886ff
moneyandjelly <rmsdldy12@gmail.com>
2024-12-11 15:43:48 +0900

    feat: [이근] Props를 사용한 디테일 다이얼로그

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -5,7 +5,7 @@ import CommonHeader from '@components/common/header/CommonHeader'
 import CommonSearchBar from '@/components/common/searchBar/CommonSearchBar'
 import CommonNav from '@/components/common/navigation/CommonNav'
 import CommonFooter from '@/components/common/footer/CommonFooter'
-import Card from './components/card'
+import Card from './components/Card'
 import DetailDialog from '@/components/common/dialog/DetailDialog'
 //CSS
 import styles from './styles/index.module.scss'
@@ -13,12 +13,12 @@ import { CardDTO } from './types/card'
 
 function index() {
     const imgSelector = useRecoilValue(imageData)
-    const [imgData, setImgData] = useState<CardDTO[]>([])
+    const [imgData, setImgData] = useState<CardDTO>()
     const [open, setOpen] = useState<boolean>(false) // 이미지 상세 다이얼로그 발생(관리) State
 
     const CARD_LIST = imgSelector.data.results.map((card: CardDTO) => {
         return (
-            <Card data={card} key={card.id} handleDialog={setOpen}/>
+            <Card data={card} key={card.id} handleDialog={setOpen} handleSetData={setImgData}/>
         )
     })
 
@@ -46,7 +46,7 @@ function index() {
             </div>
             {/* 공통 푸터 UI 부분 */}
             <CommonFooter />
-            {open && <DetailDialog />}
+            {open && <DetailDialog data={imgData} handleDialog={setOpen} />}
         </div>
     )
 }
